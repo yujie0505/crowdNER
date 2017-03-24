447a35425e1388c26384fee2f25f0e7b7fffff99
yujie <seven8355@gmail.com>
2017-03-24 16:20:31 +0800

    chore(bin): parse experiment log

--- bin/PPI/bin/src2res.js
+++ bin/PPI/bin/src2res.js
@@ -14,12 +14,33 @@ const opt = {
   srcPath : '../src'
 }
 
+// utility
+
+const parseLog = ($src, res) => {
+  for (let srcID of src[$src]) {
+    for (let log of fs.readFileSync(`${opt.srcPath}/${$src}/${srcID}/log`, 'utf-8').split('\n').slice(0, -1)) {
+      log = JSON.parse(log)
+
+      if ('submit' !== log.action) continue
+
+      let boxName = `box${log.box}`
+      let box = res[boxName] ? res[boxName] : res[boxName] = {}
+      let labeler = box[srcID] ? box[srcID] : box[srcID] = {}
+      let paper = labeler[log.pmid] ? labeler[log.pmid] : labeler[log.pmid] = {}
+      paper[log.stcid] = {
+        event   : log.event,
+        protein : log.protein
+      }
+    }
+  }
+}
+
 // load src
 
 const src = {
   ans     : fs.readdirSync(`${opt.srcPath}/ans/`),
   box     : fs.readdirSync(`${opt.srcPath}/box/`).filter(it => it.match(/^box\d+$/)),
-  log     : fs.readdirSync(`${opt.srcPath}/log/`),
+  exp     : fs.readdirSync(`${opt.srcPath}/exp/`),
   ppiPair : fs.readFileSync(`${opt.srcPath}/PINDB.tab.txt`, 'utf-8').split('\n').slice(1, -1)
 }
 
@@ -31,8 +52,6 @@ const world = {
   box  : {},
   name : 'Protein-Protein Interaction'
 }
-
-// parse subject information
 
 google.load({
   debug         : opt.debug,
@@ -47,6 +66,8 @@ google.load({
 
     delete rows[1]
 
+    // parse subject information
+
     for (let i in rows) {
       let degree_grade = rows[i][opt.sheet.degree].split('_')
       subject[rows[i][opt.sheet.expID]] = {
@@ -56,38 +77,23 @@ google.load({
         name       : rows[i][opt.sheet.name]
       }
     }
-
     fs.writeFileSync(`${opt.resPath}/subject.json`, JSON.stringify(subject, null, 2))
 
-    // parse answer
+    // parse answer logs
 
-    for (let ans of src.ans) {
-      for (let log of fs.readFileSync(`${opt.srcPath}/ans/${ans}`, 'utf-8').split('\n').slice(0, -1)) {
-        log = JSON.parse(log)
-
-        if ('submit' !== log.action) continue
-
-        let boxName = `box${log.box}`, profName = ans.replace(/\.log/, '')
-        let box = answer[boxName] ? answer[boxName] : answer[boxName] = {}
-        let prof = box[profName] ? box[profName] : box[profName] = {}
-        let paper = prof[log.pmid] ? prof[log.pmid] : prof[log.pmid] = {}
-        paper[log.stcid] = {
-          event   : log.event,
-          protein : log.protein
-        }
-      }
-    }
-
+    parseLog('ans', answer)
     fs.writeFileSync(`${opt.resPath}/answer.json`, JSON.stringify(answer, null, 2))
 
-    // parse log
+    // parse experiment logs
+
+    parseLog('exp', result)
+    fs.writeFileSync(`${opt.resPath}/result.json`, JSON.stringify(result, null, 2))
 
     // parse world information
 
     for (let box of src.box) {
       world.box[box.substring(0, 1).toUpperCase() + box.substring(1)] = {}
     }
-
     fs.writeFileSync('../res/world.json', JSON.stringify(world, null, 2))
   })
 })
