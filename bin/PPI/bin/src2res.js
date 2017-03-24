15e85ba9ec7170f090627ea327a855dfe0af2393
yujie <seven8355@gmail.com>
2017-03-24 15:23:17 +0800

    chore(bin): parse answer logs

--- bin/PPI/bin/src2res.js
+++ bin/PPI/bin/src2res.js
@@ -14,6 +14,17 @@ const opt = {
   srcPath : '../src'
 }
 
+// load src
+
+const src = {
+  ans     : fs.readdirSync(`${opt.srcPath}/ans/`),
+  box     : fs.readdirSync(`${opt.srcPath}/box/`).filter(it => it.match(/^box\d+$/)),
+  log     : fs.readdirSync(`${opt.srcPath}/log/`),
+  ppiPair : fs.readFileSync(`${opt.srcPath}/PINDB.tab.txt`, 'utf-8').split('\n').slice(1, -1)
+}
+
+const answer = {}
+const result = {}
 const subject = {}
 const world = {
   abbr : 'PPI',
@@ -48,13 +59,33 @@ google.load({
 
     fs.writeFileSync(`${opt.resPath}/subject.json`, JSON.stringify(subject, null, 2))
 
+    // parse answer
+
+    for (let ans of src.ans) {
+      for (let log of fs.readFileSync(`${opt.srcPath}/ans/${ans}`, 'utf-8').split('\n').slice(0, -1)) {
+        log = JSON.parse(log)
+
+        if ('submit' !== log.action) continue
+
+        let boxName = `box${log.box}`, profName = ans.replace(/\.log/, '')
+        let box = answer[boxName] ? answer[boxName] : answer[boxName] = {}
+        let prof = box[profName] ? box[profName] : box[profName] = {}
+        let paper = prof[log.pmid] ? prof[log.pmid] : prof[log.pmid] = {}
+        paper[log.stcid] = {
+          event   : log.event,
+          protein : log.protein
+        }
+      }
+    }
+
+    fs.writeFileSync(`${opt.resPath}/answer.json`, JSON.stringify(answer, null, 2))
+
+    // parse log
+
     // parse world information
 
-    const src = {
-      ans     : fs.readdirSync(`${opt.srcPath}/ans/`),
-      box     : fs.readdirSync(`${opt.srcPath}/box/`).filter(it => it.match(/^box\d+$/)),
-      log     : fs.readdirSync(`${opt.srcPath}/log/`),
-      ppiPair : fs.readFileSync(`${opt.srcPath}/PINDB.tab.txt`, 'utf-8').split('\n').slice(1, -1)
+    for (let box of src.box) {
+      world.box[box.substring(0, 1).toUpperCase() + box.substring(1)] = {}
     }
 
     fs.writeFileSync('../res/world.json', JSON.stringify(world, null, 2))
