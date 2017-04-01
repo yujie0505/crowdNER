180486acbb05f6eb296b1a384627fde4beb1b98d
yujie <seven8355@gmail.com>
2017-04-01 14:58:00 +0800

    chore(bin): refine src2res.js

--- bin/PPI/bin/src2res.js
+++ bin/PPI/bin/src2res.js
@@ -2,24 +2,31 @@ const fs = require('fs')
 const google = require('edit-google-spreadsheet')
 
 const opt = {
-  debug   : true,
-  google  : JSON.parse(fs.readFileSync('../../../option.json', 'utf-8')).google,
+  debug : true,
+  google : JSON.parse(fs.readFileSync('../../../option.json', 'utf-8')).google,
   resPath : '../res',
-  sheet   : {
-    degree     : 5,
-    department : 4,
-    expID      : 6,
-    name       : 2,
-    studentID  : 3
-  },
-  srcPath : '../src'
+  srcPath : '../src',
+  worksheetCol : {
+    avgSubmitTime : {
+      avgTime   : 3,
+      stcLength : 2,
+      stcValue  : 1
+    },
+    enroll : {
+      degree     : 5,
+      department : 4,
+      expID      : 6,
+      name       : 2,
+      studentID  : 3
+    }
+  }
 }
 
 // utility
 
 const parseLog = ($src, res) => {
   for (let srcID of src[$src]) {
-    for (let log of fs.readFileSync(`${opt.srcPath}/${$src}/${srcID}/log`, 'utf-8').split('\n').slice(0, -1)) {
+    for (let log of fs.readFileSync(`${opt.srcPath}/${$src}/${srcID}`, 'utf-8').split('\n').slice(0, -1)) {
       log = JSON.parse(log)
 
       if ('submit' !== log.action) continue
@@ -39,49 +46,48 @@ const parseLog = ($src, res) => {
 // load src
 
 const src = {
-  ans     : fs.readdirSync(`${opt.srcPath}/ans/`),
-  box     : fs.readdirSync(`${opt.srcPath}/box/`).filter(it => it.match(/^box\d+$/)),
-  exp     : fs.readdirSync(`${opt.srcPath}/exp/`),
-  ppiPair : fs.readFileSync(`${opt.srcPath}/PINDB.tab.txt`, 'utf-8').split('\n').slice(1, -1)
+  ans : fs.readdirSync(`${opt.srcPath}/ans/`),
+  box : fs.readdirSync(`${opt.srcPath}/box/`).filter(it => it.match(/^box\d+$/)),
+  exp : fs.readdirSync(`${opt.srcPath}/exp/`)
 }
 
 const answer = {}
 const expResult = {}
 const subject = {}
 const world = {
-  abbr : 'PPI',
-  box  : {},
-  name : 'Protein-Protein Interaction'
+  abbr    : 'PPI',
+  box     : {},
+  content : 'Protein-Protein Interaction'
 }
 
 google.load({
   debug         : opt.debug,
   oauth2        : opt.google.oauth2,
   spreadsheetId : opt.google.spreadsheet,
-  worksheetId   : opt.google.worksheet
+  worksheetId   : opt.google.worksheet.enroll
 }, (err, sheet) => {
   if (err) throw err
 
-  sheet.receive((err, rows, info) => {
+  sheet.receive((err, rows) => {
     if (err) throw err
 
     delete rows[1]
 
     // parse subject information
 
-    let idHash = {}
+    let idHash = {}, col = opt.worksheetCol.enroll
     for (let i in rows) {
-      idHash[rows[i][opt.sheet.expID]] = rows[i][opt.sheet.studentID]
-      if (subject[rows[i][opt.sheet.studentID]])
-        subject[rows[i][opt.sheet.studentID]].expID.push(rows[i][opt.sheet.expID])
+      idHash[rows[i][col.expID]] = rows[i][col.studentID]
+      if (subject[rows[i][col.studentID]])
+        subject[rows[i][col.studentID]].expID.push(rows[i][col.expID])
       else {
-        let degree_grade = rows[i][opt.sheet.degree].split('_')
-        subject[rows[i][opt.sheet.studentID]] = {
+        let degree_grade = rows[i][col.degree].split('_')
+        subject[rows[i][col.studentID]] = {
           degree     : degree_grade[0],
-          department : rows[i][opt.sheet.department],
-          expID      : [rows[i][opt.sheet.expID]],
+          department : rows[i][col.department],
+          expID      : [rows[i][col.expID]],
           grade      : degree_grade[1],
-          name       : rows[i][opt.sheet.name]
+          name       : rows[i][col.name]
         }
       }
     }
@@ -100,9 +106,8 @@ google.load({
     // parse world information
 
     for (let boxName of src.box) {
-      let box = world.box[boxName.substring(0, 1).toUpperCase() + boxName.substring(1)] = {}
+      let box = world.box[`${boxName.slice(0, 1).toUpperCase()}${boxName.slice(1, -1)} ${boxName.slice(-1)}`] = { articles: {} }
 
-      box.articles = {}
       for (let pmid of fs.readdirSync(`${opt.srcPath}/box/${boxName}`).filter(it => it.match(/\d+/)))
         box.articles[pmid] = JSON.parse(fs.readFileSync(`${opt.srcPath}/box/${boxName}/${pmid}`, 'utf-8')).title
 
@@ -120,7 +125,8 @@ google.load({
         article     : Object.keys(box.articles).length,
         expLogs     : logs,
         subject     : Object.keys(student).length,
-        twoValueStc : JSON.parse(fs.readFileSync(`${opt.srcPath}/box/${boxName}/stack`))[2].length
+        twoValueStc : JSON.parse(fs.readFileSync(`${opt.srcPath}/box/${boxName}/stack`))[2].length,
+        updateTime  : new Date().toISOString().slice(0, 10).replace(/-/g, '.')
       }
     }
     fs.writeFileSync('../res/world.json', JSON.stringify(world, null, 2))
