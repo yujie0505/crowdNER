91256e4a681b52f73ee9af81b2591a557da2fecd
yujie <seven8355@gmail.com>
2017-06-06 14:03:54 +0800

    refactor(bin): divide some logics from src2res.js and need to be combined into ans.ls

--- bin/PPI/bin/src2res.js
+++ bin/PPI/bin/src2res.js
@@ -3,7 +3,6 @@ const google = require('edit-google-spreadsheet')
 
 const opt = {
   debug  : true,
-  entity : ['event', 'protein'],
   google : JSON.parse(fs.readFileSync('../../../option.json', 'utf-8')).google,
   resPath : '../res',
   srcPath : '../src',
@@ -25,57 +24,8 @@ const opt = {
 // load src
 
 const src = {
-  ans : fs.readdirSync(`${opt.srcPath}/ans/`),
   art : {},
-  box : fs.readdirSync(`${opt.srcPath}/box/`).filter(it => it.match(/^box\d+$/)),
-  exp : fs.readdirSync(`${opt.srcPath}/exp/`)
-}
-
-// utility
-
-const labeledStc = {}
-const parseLog = ($src, res) => {
-  for (let srcID of src[$src]) {
-
-    let lastSubmitTime = null, elapsedTime = 0
-    for (let log of fs.readFileSync(`${opt.srcPath}/${$src}/${srcID}`, 'utf-8').split('\n').slice(0, -1)) {
-      log = JSON.parse(log)
-
-      let currSubmitTime = new Date(log.time) / 1000
-      if (lastSubmitTime) elapsedTime = currSubmitTime - lastSubmitTime
-      lastSubmitTime = currSubmitTime
-
-      if ('submit' !== log.action) continue
-
-      // parse submit logs
-
-      let boxName = `box${log.box}`
-      let box = res[boxName] ? res[boxName] : res[boxName] = { submits: 0 }
-      let labeler = box[srcID] ? box[srcID] : box[srcID] = {}
-      let article = labeler[log.pmid] ? labeler[log.pmid] : labeler[log.pmid] = {}
-      let sentence = article[log.stcid] ? article[log.stcid] : article[log.stcid] = {
-        elapsedTime : elapsedTime,
-        event : {},
-        protein : {}
-      }
-
-      box.submits++
-      for (let e of opt.entity)
-        for (let i of log[e])
-          sentence[e][i] = true
-
-      if ('ans' === $src) continue
-
-      // statistic of sentences labeled by subjects
-
-      box = labeledStc[boxName] ? labeledStc[boxName] : labeledStc[boxName] = {}
-      let art = box[log.pmid] ? box[log.pmid] : box[log.pmid] = {}
-      let stc = art[log.stcid] ? art[log.stcid] : art[log.stcid] = { elapsedTime: [], labeler: {} }
-      if (stc.labeler[srcID]) continue // repeated labeled
-      if (elapsedTime) stc.elapsedTime.push(elapsedTime)
-      stc.labeler[srcID] = true
-    }
-  }
+  box : fs.readdirSync(`${opt.srcPath}/box/`).filter(it => it.match(/^box\d+$/))
 }
 
 google.load({
@@ -93,10 +43,9 @@ google.load({
 
     // parse subject information
 
-    const idHash = {}, subject = {}
+    const subject = {}
     let col = opt.worksheetCol.enroll
     for (let i in rows) {
-      idHash[rows[i][col.expID]] = rows[i][col.studentID]
       if (subject[rows[i][col.studentID]])
         subject[rows[i][col.studentID]].expID.push(rows[i][col.expID])
       else {
@@ -112,18 +61,6 @@ google.load({
     }
     fs.writeFileSync(`${opt.resPath}/world/subject.json`, JSON.stringify(subject, null, 2))
 
-    // parse answer logs
-
-    const answer = {}
-    parseLog('ans', answer)
-    fs.writeFileSync(`${opt.resPath}/answer.json`, JSON.stringify(answer, null, 2))
-
-    // parse experiment logs
-
-    const expResult = {}
-    parseLog('exp', expResult)
-    fs.writeFileSync(`${opt.resPath}/result.json`, JSON.stringify(expResult, null, 2))
-
     // parse world information
 
     const stcValue = {}, world = Object.assign({}, opt.world, { box: {} })
@@ -135,10 +72,6 @@ google.load({
         src.art[boxName][pmid] = JSON.parse(fs.readFileSync(`${opt.srcPath}/box/${boxName}/${pmid}`, 'utf-8'))
         box.articles[pmid] = src.art[boxName][pmid].title
       }
-
-      let student = {}
-      for (let expID in expResult[boxName])
-        student[idHash[expID]] = true
 
       let boxStack = JSON.parse(fs.readFileSync(`${opt.srcPath}/box/${boxName}/stack`))
 
@@ -153,66 +86,16 @@ google.load({
       }
 
       box.statistic = {
-        answers  : answer[boxName] ? Object.keys(answer[boxName]).length - 1 : 0,
         articles : Object.keys(box.articles).length,
         stcValue : {
           '0' : boxStack[0].length,
           '1' : boxStack[1].length,
           '2' : boxStack[2].length
         },
-        subjects   : Object.keys(student).length,
-        submits    : expResult[boxName] ? expResult[boxName].submits : 0,
         updateTime : new Date().toISOString().slice(0, 10).replace(/-/g, '.')
       }
     }
     fs.writeFileSync(`${opt.resPath}/world/world.json`, JSON.stringify(world, null, 2))
     fs.writeFileSync(`${opt.resPath}/world/stcValue.json`, JSON.stringify(stcValue, null, 2))
-
-    // record labeled sentence
-
-    for (let boxName in labeledStc) {
-      for (let pmid in labeledStc[boxName]) {
-        for (let stcid in labeledStc[boxName][pmid]) {
-          let labels = labeledStc[boxName][pmid][stcid].labels = {}
-          for (let expID in labeledStc[boxName][pmid][stcid].labeler) {
-            for (let entity of opt.entity) {
-              for (let wid in expResult[boxName][expID][pmid][stcid][entity]) {
-                let wordLabel = labels[wid] ? labels[wid] : labels[wid] = { event: 0, protein: 0 }
-                wordLabel[entity]++
-              }
-            }
-          }
-
-          labeledStc[boxName][pmid][stcid].supp = Object.keys(labeledStc[boxName][pmid][stcid].labeler).length
-        }
-      }
-    }
-    fs.writeFileSync(`${opt.resPath}/world/labeledStc.json`, JSON.stringify(labeledStc, null, 2))
-
-    // parse top frequent words
-
-    const topFreqWords = {}
-    for (let boxName in src.art) {
-      topFreqWords[boxName] = {}
-
-      for (let pmid in src.art[boxName]) {
-        let likelihood = src.art[boxName][pmid].likelihood, words = {}
-        topFreqWords[boxName][pmid] = []
-
-        for (let stcid in src.art[boxName][pmid].word) {
-          for (let wid in src.art[boxName][pmid].word[stcid]) {
-            if (0 === parseFloat(likelihood.event[stcid][wid]) && 0 === parseFloat(likelihood.protein[stcid][wid])) continue // stop words
-
-            let word = words[src.art[boxName][pmid].word[stcid][wid]] ? words[src.art[boxName][pmid].word[stcid][wid]] : words[src.art[boxName][pmid].word[stcid][wid]] = { frequency: 0, stcid: {} }
-            word.frequency++, word.stcid[stcid] = true
-          }
-        }
-
-        for (let word of Object.keys(words).sort((a, b) => words[b].frequency - words[a].frequency)) {
-          topFreqWords[boxName][pmid].push(Object.assign({ word: word }, words[word]))
-        }
-      }
-    }
-    fs.writeFileSync(`${opt.resPath}/world/wordsFreq.json`, JSON.stringify(topFreqWords, null, 2))
   })
 })
