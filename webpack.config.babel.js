04037c478bfd4dda34e9c115b65e207aff8acd04
yujie <seven8355@gmail.com>
2017-07-12 11:16:53 +0800

    docs(webpack): refine and new modules

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,95 +1,33 @@
-import chalk from 'chalk'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
-import fs from 'fs'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import path from 'path'
-import ProgressBarPlugin from 'progress-bar-webpack-plugin'
+import autoprefixer from 'autoprefixer'
+import { resolve } from 'path'
 import webpack from 'webpack'
 
 // config
-const opt = JSON.parse(fs.readFileSync('./option.json'))
+import opt from './option.json'
 
 export default {
+  context: resolve('app'),
   devServer: {
-    contentBase: path.resolve('./dist'),
     host: opt.host,
-    hot: true,
     inline: true,
     port: opt.port,
-    stats: {
-		  chunkModules: false,
-		  chunks: false,
-		  colors: true,
-		  hash: false,
-		  version: false
-    }
+    stats: { chunkModules: false }
   },
-  entry: path.resolve('./app/app.js'),
+  entry: './app.js',
   module: {
     rules: [
-      {
-        test: /\.(jpg|jpeg|png)$/,
-        use: 'url-loader?limit=10000'
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
-      },
-      {
-        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        use: 'file-loader?name=fonts/[name].[ext]'
-      },
-      {
-        test: /\.js$/,
-        exclude: /\/node_modules\/|\/lib\//,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.pug$/,
-        use: 'pug-loader'
-      },
-      {
-        test: /\.styl$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            'css-loader',
-            'stylus-loader'
-          ]
-        })
-      }
+      { test: /\.css$/, use: ['style-loader?insertAt=top', 'css-loader'] },
+      { test: /\.(eot|otf|svg|ttf|woff(2)?)(\?[a-z0-9]+)?$/, use: 'file-loader?name=fonts/[hash:7].[ext]' },
+      { test: /\.(jpeg|jpg|png)$/, use: 'url-loader?limit=10000' },
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+      { test: /\.pug$/, use: ['file-loader?name=[name].html', 'extract-loader', 'html-loader', 'pug-html-loader?exports=false'] },
+      { test: /\.sass$/, use: ['file-loader?name=[name].css', 'extract-loader', 'css-loader', { loader: 'postcss-loader', options: { plugins: [autoprefixer] } }, 'sass-loader'] }
     ]
   },
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: 'app.css'
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve('./app/index.pug')
-    }),
-    new ProgressBarPlugin({
-      format: `:msg build [${chalk.yellow.bold(':bar')}] ${chalk.green.bold(':percent')} (:elapsed seconds)`
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.LoaderOptionsPlugin({
-      stylus: {
-        default: {
-          use: [require('nib')()]
-        }
-      }
-    }),
-    new webpack.NoEmitOnErrorsPlugin()
-  ],
-  resolve: {
-    modules: [
-      'node_modules'
-    ]
+    path: resolve('dist')
   }
 }
+
+// vi:et
